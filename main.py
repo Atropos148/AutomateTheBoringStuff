bed42672e5aef11a42d20318a201aef860b0ad12
atropos148 <lacroix148@gmail.com>
2016-01-17 16:07:29 +0100

    hello world

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 __author__ = 'Atropos'
+
+
+print('hello world!')
+print('What is your name?')
+myName = input()
+print('Good to meet you ' + myName)
+print('Lenght of your name is:')
+print(len(myName))
+print('Your age?')
+myAge = input()
+print('you will be ' + str(int(myAge) + 1) + ' in a year.')
