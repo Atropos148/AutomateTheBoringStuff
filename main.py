33133fa6a8ce97ab0e624a7bda3a0aff508ac353
atropos148 <lacroix148@gmail.com>
2016-01-17 17:19:31 +0100

    no message

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-__author__ = 'Atropos'
+spam = ['apples', 'bananas', 'tofu', 'cats']
+hugeList = ['car','house','cat','furry','bet','sack','high','like','Mike']
+print('Lists:')
+print('1.spam' + ' ' + '2.hugeList')
+while True:
+    print('What list do you want?')
+    choice = input()
+    if choice == '1':
+        list = spam
+        break
+    elif choice == '2':
+        list = hugeList
+        break
+    else:
+        print('Bad input')
 
 
-print('hello world!')
-print('What is your name?')
-myName = input()
-print('Good to meet you ' + myName)
-print('Lenght of your name is:')
-print(len(myName))
-print('Your age?')
-myAge = input()
-print('you will be ' + str(int(myAge) + 1) + ' in a year.')
+for i in range(len(list)):
+    if i == (len(list)-1):
+        print(list[i],end='')
+    elif i == (len(list)-2):
+        print(list[i],end=' and ')
+    elif i < len(list):
+        print(list[i],end=', ')
