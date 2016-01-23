022761288917b7c6855b8ca0d933cfc3f82e7cde
Tibor Hrdlovic <lacroix148@gmail.com>
2016-01-23 13:31:38 +0100

    pef.bat added EU phone numbers

--- phoneEmailFinder.py
+++ phoneEmailFinder.py
@@ -2,7 +2,7 @@
 
 import re, pyperclip
 
-phoneRegex = re.compile(r'''(
+USphoneRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\))?  # area code(optional)
     (\s|-|\.)?  # separator
     (\d{3}) # first 3 numbers
@@ -10,6 +10,16 @@ phoneRegex = re.compile(r'''(
     (\d{4}) # last 4 numbers
     (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension(optional)
     )''', re.VERBOSE)
+
+EUphoneRegex = re.compile(r'''(
+    (\d{4}|\(\d{4}\))  # area code(optional)
+    (\s|-|\.)?  # separator
+    (\d{3}) # first 3 numbers
+    (\s|-|\.) # separator
+    (\d{3}) # last 4 numbers
+    (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension(optional)
+    )''', re.VERBOSE)
+
 
 emailRegex = re.compile(r'''(
     [a-zA-Z0-9._+-]+ #username
@@ -21,16 +31,22 @@ emailRegex = re.compile(r'''(
 # Find matches in clipboard text.
 text = str(pyperclip.paste())
 matches = []
-for groups in phoneRegex.findall(text):
+for groups in USphoneRegex.findall(text):
     phoneNum = '-'.join([groups[1], groups[3], groups[5]])
     if groups[8] != '':
         phoneNum += ' x' + groups[8]
     matches.append(phoneNum)
 
+for groups in EUphoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+
+
 for groups in emailRegex.findall(text):
     matches.append(groups[0])
 
-# TODO: Copy results to the clipboard.
 if len(matches) > 0:
     pyperclip.copy('\n'.join(matches))
     print('Copied to clipboard:')
